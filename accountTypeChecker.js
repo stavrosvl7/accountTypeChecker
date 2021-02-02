7e0c8cb1b772e5967cc4c4ec315b699c6d60a1a8
Stavrosvl7 <stavrosvl7@gmail.com>
2021-02-02 23:42:58 +0200

    Update accountTypeChecker.js

--- accountTypeChecker.js
+++ accountTypeChecker.js
@@ -2,19 +2,19 @@ const accountBalanceHistory = [
   {
     monthNumber: 0, // current month
     account: {
-      balance: { amount: 100 },
+      balance: { amount: 0 },
     },
   },
   {
     monthNumber: 1, // last month
     account: {
-      balance: { amount: 250 },
+      balance: { amount: 100 },
     },
   },
   {
     monthNumber: 2, // two months ago
     account: {
-      balance: { amount: 300 },
+      balance: { amount: 200 },
     },
   }
 ]
